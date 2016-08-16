8ea068272f4fc0fe2508ff6329fbc96c60cbbca2
Nissy0409 <i.wish.glory.reds@gmail.com>
2016-08-17 02:30:25 +0900

    initial commit

---
+++ src/main/java/com/fileeeeed/FileFeeeedApplication.java
@@ -0,0 +1,12 @@
+package com.fileeeeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileFeeeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileFeeeedApplication.class, args);
+	}
+}
